668ab3a53cbd811b9759fabcf9169e724b4b09ed
dessoukym <167462232+dessoukym@users.noreply.github.com>
2026-08-25 14:36:28 +0300

    dashboard for bussinesses

--- Frontend/Recyclehub/src/App.tsx
+++ Frontend/Recyclehub/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
     </Routes>
   )
 }
